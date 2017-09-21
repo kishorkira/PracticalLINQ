3a9397178fcb17052373ca3ba83d9bf726d0b448
kishorkira <kishorkira@gmail.com>
2017-09-21 19:14:16 +0530

    Added Unit test project for Basic

---
+++ Basics.Test/CustomerRepositoryTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics.Test
+{
+    [TestClass]
+    class CustomerRepositoryTest
+    {
+        public TestContext TestContext { get; set; }
+        [TestMethod]
+        public void FindExistingCustomerTest()
+        {
+
+        }
+    }
+}
+    
---
+++ Basics.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Basics.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Basics.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ded2c44-4ddf-4d82-ad75-c8334a87fd8d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
