fb0539e5140951dd76ce3e17437e964c58d64d83
kishorkira <kishorkira@gmail.com>
2017-09-23 10:40:41 +0530

    Added CustomerType and CustomerTypeRepository

---
+++ Basics/CustomerTypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Basics
+{
+    class CustomerTypeRepository
+    {
+        public List<CustomerType> Retrive()
+        {
+            List<CustomerType> customerTypesList = new List<CustomerType>
+            {
+                new CustomerType{CustomerTypeId = 1, TypeName = "Corporate" ,DisplayOrder = 2},
+                new CustomerType{CustomerTypeId = 2, TypeName = "Individual" ,DisplayOrder = 1},
+                new CustomerType{CustomerTypeId = 3, TypeName = "Educator" ,DisplayOrder = 4},
+                new CustomerType{CustomerTypeId = 4, TypeName = "Government" ,DisplayOrder = 3},
+
+            };
+            return customerTypesList;
+        }
+    }
+}
